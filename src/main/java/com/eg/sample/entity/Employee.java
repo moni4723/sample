bdf85d413f53e4d7760d2e1756dec22cd6efffaa
Monica B <bmonica4723@gmail.com>
2025-02-04 21:35:05 +0530

    Initial commit

--- src/main/java/com/eg/sample/entity/Employee.java
+++ src/main/java/com/eg/sample/entity/Employee.java
@@ -13,7 +13,7 @@ public class Employee {
 
     // Getters and Setters
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
